2942ccd801dbf7750bd80a168d1c9f8997430f31
Kyle Suss <susskyle@gmail.com>
2019-12-20 11:52:39 -0700

    Add parameters

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,8 +1,13 @@
 import { document, console } from "global";
 import { storiesOf } from "@storybook/html";
 
+const VIEWPORT_XSMALL = '400'
+const VIEWPORT_SMALL = '768'
+const VIEWPORT_MEDIUM = '1280'
+
 export default {
   title: "Demo"
+  parameters: { chromatic: { viewports: [VIEWPORT_XSMALL, VIEWPORT_SMALL, VIEWPORT_MEDIUM] }}
 };
 
 export const HeadingLevel1 = () => "<h1>Hello World</h1>";
