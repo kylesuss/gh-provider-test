6993ecbc3649e5f85f8c0d537b98518aa80c4b70
Kyle Suss <susskyle@gmail.com>
2019-12-20 09:55:01 -0700

    Path separators

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,13 +2,16 @@ import { document, console } from "global";
 import { storiesOf } from "@storybook/html";
 
 storiesOf("Demo", module)
-  .add("heading level 1", () => "<h1>Hello World</h1>")
-  .add("heading level 2", () => "<h2>Hello World</h2>")
-  .add("heading level 3", () => "<h3>Hello World</h3>")
-  .add("heading level 4", () => "<h4>Hello World</h4>")
-  .add("heading level 5", () => "<h5>Hello World</h5>")
-  .add("paragraph", () => "The quick brown fox jumped over the lazy dog.")
-  .add("button", () => {
+  .add("Test|Atoms/Demo/heading level 1", () => "<h1>Hello World</h1>")
+  .add("Test|Atoms/Demo/heading level 2", () => "<h2>Hello World</h2>")
+  .add("Test|Atoms/Demo/heading level 3", () => "<h3>Hello World</h3>")
+  .add("Test|Atoms/Demo/heading level 4", () => "<h4>Hello World</h4>")
+  .add("Test|Atoms/Demo/heading level 5", () => "<h5>Hello World</h5>")
+  .add(
+    "Test|Atoms/Demo/paragraph",
+    () => "The quick brown fox jumped over the lazy dog."
+  )
+  .add("Test|Atoms/Demo/button", () => {
     const button = document.createElement("button");
     button.type = "button";
     button.style = "color:green;";
