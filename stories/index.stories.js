5d3d6f4c343152107f9a0c176a33db9c292c8f2e
Kyle Suss <susskyle@gmail.com>
2020-09-10 14:21:04 -0600

    Change to red

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -11,7 +11,7 @@ storiesOf("Demo", module)
   .add("button", () => {
     const button = document.createElement("button");
     button.type = "button";
-    button.style = "color:green;";
+    button.style = "color:red;";
     button.innerText = "Hello Button";
     button.addEventListener("click", (e) => console.log(e));
     return button;
