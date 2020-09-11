cce38323d023736ee7728b45db6daed248038e23
Kyle Suss <susskyle@gmail.com>
2020-09-11 15:21:54 -0600

    Will this work

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -12,7 +12,7 @@ storiesOf("Demo", module)
     const button = document.createElement("button");
     button.type = "button";
     button.style = "color:blue;";
-    button.innerText = "Derp";
+    button.innerText = "Will this work on a rebase merge?";
     button.addEventListener("click", (e) => console.log(e));
     return button;
   });
