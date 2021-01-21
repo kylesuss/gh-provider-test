f159b59ae7c5b4da411d971f3614627b45e8ebb8
Kyle Suss <susskyle@gmail.com>
2021-01-21 15:55:49 -0700

    Update to CSF

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,18 +1,20 @@
-import { document, console } from "global";
-import { storiesOf } from "@storybook/html";
+import { document } from "global";
 
-storiesOf("Demo", module)
-  .add("heading level 1", () => "<h1>Hello World</h1>")
-  .add("heading level 2", () => "<h2>Hello World</h2>")
-  .add("heading level 3", () => "<h3>Hello World</h3>")
-  .add("heading level 4", () => "<h4>Hello World</h4>")
-  .add("heading level 5", () => "<h5>Hello World</h5>")
-  .add("paragraph", () => "The quick brown fox jumped over the lazy dog.")
-  .add("button", () => {
-    const button = document.createElement("button");
-    button.type = "button";
-    button.style = "color:green;";
-    button.innerText = "Last chance baby!!";
-    button.addEventListener("click", (e) => console.log(e));
-    return button;
-  });
+export default {
+  title: 'Test'
+}
+
+export const HeadingLevel1 = () => "<h1>Hello World</h1>"
+export const HeadingLevel2 = () => "<h2>Hello World</h2>"
+export const HeadingLevel3 = () => "<h3>Hello World</h3>"
+export const HeadingLevel4 = () => "<h4>Hello World</h4>"
+export const HeadingLevel5 = () => "<h5>Hello World</h5>"
+export const Paragraph = () => "<p>The quick brown fox jumped over the lazy dog.</p>"
+export const Button = () => {
+  const button = document.createElement("button");
+  button.type = "button";
+  button.style = "color:green;";
+  button.innerText = "Last chance baby!!";
+  button.addEventListener("click", (e) => console.log(e));
+  return button;
+}
