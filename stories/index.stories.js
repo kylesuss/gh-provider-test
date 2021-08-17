7466b74cf852a4e0ec15962213a76abb839eedd6
Kyle Suss <susskyle@gmail.com>
2021-08-17 15:44:11 -0600

    Tall thing

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,7 +4,7 @@ export default {
   title: 'Test'
 }
 
-export const HeadingLevel1 = () => "<h1>Hello World</h1>"
+export const HeadingLevel1 = () => "<h1>Hello Worldz</h1>"
 export const HeadingLevel2 = () => "<h2>Hello World</h2>"
 export const HeadingLevel3 = () => "<h3>Hello World</h3>"
 export const HeadingLevel4 = () => "<h4>Hello World</h4>"
@@ -21,8 +21,8 @@ export const Button = () => {
 
   const button = document.createElement("button");
   button.type = "button";
-  button.style = "color:red;";
-  button.innerText = "I am button!!";
+  button.style = "color:purple;";
+  button.innerText = "I am buttonz!!";
   button.addEventListener("click", (e) => console.log(e));
 
   const ignored = document.createElement("div");
@@ -38,4 +38,10 @@ Button.story = {
   parameters: {
     chromatic: { viewports: [600, 1200] }
   }
+}
+
+export const TallThing = () => {
+  const div = document.createElement("div");
+  div.style = "width: 400px; height: 2000px; background: red;";
+  return div;
 }
---
+++ stories/new.stories.js
@@ -0,0 +1,7 @@
+import { document } from "global";
+
+export default {
+  title: 'New'
+}
+
+export const HeadingLevel1 = () => "<h1>Hello Worldz</h1>"
